8bb06714cf72353a9703d3058b3280a262f39f00
MoonlightPress <scott@moonlight-press.com>
2023-11-13 18:35:48 -0800

    Added winter tiles

---
+++ Assets/Frontier/Maps/tf_winter_tileA2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tf_winter_tileA2" tilewidth="32" tileheight="32" margin="16" tilecount="391" columns="23">
+ <image source="../Sprites/tf_winter_tileA2.png" width="768" height="576"/>
+</tileset>
