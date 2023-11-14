50d65276b4b9f3fcebd82daa159ff93091ebe76d
CharmedSatyr <16056963+CharmedSatyr@users.noreply.github.com>
2023-11-14 11:08:34 -0800

    dd obstacles that disappear with enough carolers following.

---
+++ Assets/Scripts/CarolerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Linq;
+
+public class CarolerController : MonoBehaviour
+{
+    public static int CarolerCount { get; private set; } = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"Current caroler count: ${CarolerCount}");
+        CarolerCount = GetActiveCarolerCount();
+    }
+
+    private int GetActiveCarolerCount()
+    {
+        Caroler[] carolers = FindObjectsOfType<Caroler>();
+
+        return carolers.Where(c => c.isActive).Count();
+    }
+}
